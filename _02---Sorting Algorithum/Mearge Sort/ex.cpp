9dc30cc6f8db1e3ce734f08138746264d6713360
Vineet Singraul <singraulvineet943@gmail.com>
2025-02-14 10:59:23 +0530

    new code

--- _02---Sorting Algorithum/Mearge Sort/ex.cpp
+++ _02---Sorting Algorithum/Mearge Sort/ex.cpp
@@ -1,67 +1,84 @@
 #include <iostream>
 using namespace std;
-void mearges(int A[] , int low ,int mid , int heigh)
+
+void merges(int A[], int low, int mid, int heigh)
 {
-    int i , j , k , B[heigh+1];
+    int i, j, k, B[heigh - low + 1];
     i = low;
-    j = mid+1;
-    k =low;
+    j = mid + 1;
+    k = 0;
 
-    while(i<=mid && j<=heigh)
+    while (i <= mid && j <= heigh)
     {
-        if(A[i] < A[j])
+        if (A[i] < A[j])
         {
             B[k] = A[i];
             i++;
         }
-        else{
-            B[k] = A[i];
-            k++;
-            i++;
+        else
+        {
+            B[k] = A[j];
+            j++;
         }
-
-
-       while(j <= heigh)
-       {
-        B[k] = A[j];
         k++;
+    }
+
+    while (i <= mid)
+    {
+        B[k] = A[i];
+        i++;
+        k++;
+    }
+
+    while (j <= heigh)
+    {
+        B[k] = A[j];
         j++;
-       }
-
-       for(i=low ; i<=heigh ; i++)
-       {
-        A[i] = B[i];
-       }
+        k++;
     }
 
-    void divide(int A[] , int low , int heigh)
+    for (i = low, k = 0; i <= heigh; i++, k++)
     {
-        int mid;
-        if(low < heigh)
-        {
-            mid = (low + heigh) / 2;
-            divide(A,low,mid);
-            divide(A,mid+1,heigh);
-            mearges(A,low,mid,heigh);
-        }
+        A[i] = B[k];
     }
-
-    void printarr(int A[] , int n)
-    {
-        int i;
-        for(i =0 ; i<n ; i++)
-        {
-            cout<<A[i]<<"\t";
-        }
-        cout<<"\n";
-    }a
-
 }
 
 
+void divide(int A[], int low, int heigh)
+{
+    int mid;
+    if (low < heigh)
+    {
+        mid = (low + heigh) / 2;
+        divide(A, low, mid);
+        divide(A, mid + 1, heigh);
+        merges(A, low, mid, heigh);
+    } 
+}
+
+void printarr(int A[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        cout << A[i] << "\t";
+    }
+    cout << "\n";
+}
+
 int main()
 {
-    int A[] = {3,5,6,8,4,5,7,6,3};
+    int A[] = {5,4,3,1,2};
+    // int A[] = {3, 5, 6, 8, 4, 5, 7, 6, 3};
     int n = sizeof(A) / sizeof(A[0]);
-    pri
+
+    cout << "------------- BEFORE MERGE SORT -------------\n";
+    printarr(A, n);
+
+    divide(A, 0, n - 1);
+
+    cout << "------------- AFTER MERGE SORT -------------\n";
+    printarr(A, n);
+
+    return 0;
 }
--- _02---Sorting Algorithum/QK_Sort()/ex..cpp
+++ _02---Sorting Algorithum/QK_Sort()/ex..cpp
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 using namespace std;
 
