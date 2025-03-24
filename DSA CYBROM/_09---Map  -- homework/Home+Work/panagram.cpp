08dea610c5cbb6f3de7c46607f548a8d2f9522c5
Vineet Singraul <singraulvineet943@gmail.com>
2025-03-24 11:41:48 +0530

    new code

---
+++ DSA CYBROM/_09---Map  -- homework/Home+Work/panagram.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+class Panagram
+{
+    public:
+    bool Checkpanagram(string ch)
+    {
+        unordered_set<char> latters;
+
+        for(auto c : ch)
+        {
+            if(c >= 'a' && c <='z')
+            {
+                latters.insert(c);
+            }
+        }
+        return latters.size() == 26;
+    }
+};
+
+int main()
+{
+    Panagram obj1;
+    string ch = "The quick brown fox jumps over the lazy dog";
+    bool f = obj1.Checkpanagram(ch);
+
+    if(f)
+    {
+        cout<<"Panagram : ";
+    }
+    else{
+        cout<<"not Panagram :";
+    }
+}
