0340dd6230fd3f377c1630b3b22d788e13ea43b2
Vineet Singraul <singraulvineet943@gmail.com>
2025-04-06 23:27:41 +0530

    new code

---
+++ DSA-Home/_08---Map/example3.cpp
@@ -0,0 +1,43 @@
+// 1. Insert and Retrieve Values in a Map
+// Given a list of key-value pairs, insert them into a map<int, string> and retrieve a value for a given key.
+
+// Input:  5
+// 1) Alice    2) Bob   3) Charlie   4) David    5) Eve
+// Output:  Charlie
+
+#include <iostream>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+  int n, roll;
+  string name;
+  map<int, string> mp;
+  cout << "Enter Size Of MAp : ";
+  cin >> n;
+
+  for (int i = 0; i < n; ++i)
+  {
+    cout << "Enter Name : " << i + 1 << " = ";
+    cin >> name;
+    cout << "Enter Roll Number " << i + 1 << "= ";
+    cin >> roll;
+    mp[roll] = name;
+  }
+
+  for (auto f : mp)
+  {
+    cout << "Name is : " << f.first << " : " << " roll number is :" << f.second << endl;
+  }
+
+  cout << endl;
+  cout << "-----------------------" << endl;
+  int vala;
+  cout << "Enter Value Want To Search : ";
+  cin >> vala;
+  auto f = mp.find(vala);
+  cout << "hare  IS : " << f->second;
+}
